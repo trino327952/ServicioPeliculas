26b35f07f64f5550b71cf06cdd3ca11d0521cd0b
maronv64 <maron.v64@gmail.com>
2020-01-19 01:52:29 -0500

    dgdfd

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('tipousuario/','TipoUsuarioController');
+Route::resource('usuario/','UserController');
